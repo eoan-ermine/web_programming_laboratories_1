e299f101303a609c1a98adeac732334bab383fd6
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-09-30 13:22:33 +0300

    Simplify task 4 solution

--- web_programming_laboratories_1/task4.py
+++ web_programming_laboratories_1/task4.py
@@ -1,9 +1,6 @@
-import math
-import ast
-
 def main():
-	L = input("Введите список слов (каждое слово должно быть в кавычках): ")
-	L = ast.literal_eval(f"[{', '.join(L.split(','))}]")
+	L = input("Введите список слов: ")
+	L = L.split(",")
 	L = map(lambda x: x.strip(), L)
 	L = map(lambda x: x.lower(), L)
 	print(str(list(L)))
